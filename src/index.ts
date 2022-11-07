461c726805b06e052a7045c65bdd357bc2a34132
Duong Thanh Duy <duydt@hasaki.vn>
2022-11-07 13:32:05 +0700

    fix redis

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
-require('dotenv').config();
-import express, { Response } from 'express';
-import { getMetadata } from './lib';
-import { checkForCache, createCache } from './lib/cache';
-import { errorResponse, successResponse } from './lib/response';
-import { APIOutput } from './types';
+require("dotenv").config();
+import express, { Response } from "express";
+import { getMetadata } from "./lib";
+import { checkForCache, createCache } from "./lib/cache";
+import { errorResponse, successResponse } from "./lib/response";
+import { APIOutput } from "./types";
 
 const app = express();
 
 const port = Number(process.env.PORT || 8080);
 
 if (process.env.REDISTOGO_URL) {
-  var rtg = require('url').parse(process.env.REDISTOGO_URL);
-  var redis = require('redis').createClient(rtg.port, rtg.hostname);
+  // var rtg = require('url').parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient({
+    url: process.env.REDISTOGO_URL,
+  });
 
-  redis.auth(rtg.auth.split(':')[1]);
+  // redis.auth(rtg.auth.split(':')[1]);
 } else {
-  var redis = require('redis').createClient();
+  var redis = require("redis").createClient();
 }
 
-const limiter = require('express-limiter')(app, redis);
+const limiter = require("express-limiter")(app, redis);
 
 limiter({
-  path: '/',
-  method: 'get',
-  lookup: ['connection.remoteAddress'],
+  path: "/",
+  method: "get",
+  lookup: ["connection.remoteAddress"],
   // 300 requests per minute
   total: 300,
   expire: 1000 * 60,
@@ -31,24 +33,24 @@ limiter({
 
 const sendResponse = (res: Response, output: APIOutput | null) => {
   if (!output) {
-    const data:APIOutput = {
-      title : null,
-      description : null,
-      image : null,
-      sitename : null,
-      hostname : null,
+    const data: APIOutput = {
+      title: null,
+      description: null,
+      image: null,
+      sitename: null,
+      hostname: null,
     };
 
     return res
-      .set('Access-Control-Allow-Origin', '*')
-      .set('Cache-Control', 'public, max-age=2592000')
+      .set("Access-Control-Allow-Origin", "*")
+      .set("Cache-Control", "public, max-age=2592000")
       .status(200)
-      .json(errorResponse(data,404,"Not found"));
+      .json(errorResponse(data, 404, "Not found"));
   }
 
   return res
-    .set('Access-Control-Allow-Origin', '*')
-    .set('Cache-Control', 'public, max-age=2592000')
+    .set("Access-Control-Allow-Origin", "*")
+    .set("Cache-Control", "public, max-age=2592000")
     .status(200)
     .json(successResponse(output));
 };
@@ -57,30 +59,30 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/v2', async (req, res) => {
+app.get("/v2", async (req, res) => {
   const url = req.query.url as unknown as string;
   const metadata = await getMetadata(url);
   return res
-    .set('Access-Control-Allow-Origin', '*')
-    .set('Cache-Control', 'public, max-age=2592000')
+    .set("Access-Control-Allow-Origin", "*")
+    .set("Cache-Control", "public, max-age=2592000")
     .status(200)
     .json({ metadata });
 });
 
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     let url = req.query.url as unknown as string;
 
     if (!url) {
       return res
-        .set('Access-Control-Allow-Origin', '*')
+        .set("Access-Control-Allow-Origin", "*")
         .status(400)
-        .json({ error: 'Invalid URL' });
+        .json({ error: "Invalid URL" });
     }
 
-    url = url.indexOf('://') === -1 ? 'http://' + url : url;
+    url = url.indexOf("://") === -1 ? "http://" + url : url;
 
     const isUrlValid =
       /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi.test(
@@ -89,9 +91,9 @@ app.get('/', async (req, res) => {
 
     if (!url || !isUrlValid) {
       return res
-        .set('Access-Control-Allow-Origin', '*')
+        .set("Access-Control-Allow-Origin", "*")
         .status(400)
-        .json({ error: 'Invalid URL' });
+        .json({ error: "Invalid URL" });
     }
 
     if (url && isUrlValid) {
@@ -121,14 +123,14 @@ app.get('/', async (req, res) => {
         : meta.description
         ? meta.description
         : null;
-      const title = (og.title ? og.title : meta.title) || '';
-      const siteName = og.site_name || '';
+      const title = (og.title ? og.title : meta.title) || "";
+      const siteName = og.site_name || "";
 
       output = {
         title,
         description,
         image,
-        sitename : siteName,
+        sitename: siteName,
         hostname,
       };
 
@@ -144,9 +146,11 @@ app.get('/', async (req, res) => {
       }
 
       return sendResponse(res, output);
-
     }
   } catch (error) {
-    return res.set('Access-Control-Allow-Origin', '*').status(500).json(errorResponse(null,500,"Error"));
+    return res
+      .set("Access-Control-Allow-Origin", "*")
+      .status(500)
+      .json(errorResponse(null, 500, "Error"));
   }
 });
