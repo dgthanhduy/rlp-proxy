0dbec2d77456c16eada1b0a03e7b40a45abe9506
Duong Thanh Duy <duydt@hasaki.vn>
2022-11-16 08:23:09 +0700

    update cache

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -8,24 +8,25 @@ const supabase = createClient(
   process.env.SUPABASE_KEY!
 );
 
+const cacheKeyPrefix = 'services:linkpreview:'
+
+const redis = require("redis").createClient({
+  url: process.env.REDISTOGO_URL,
+});
+
 interface CacheRecord extends APIOutput {
   url: string;
 }
 
 const checkForCache = async (url: string): Promise<APIOutput | null> => {
   try {
-    let { data, error } = await supabase
-      .from("meta-cache")
-      .select("*")
-      .eq("url", url);
 
-    if (error) {
-      console.log(error);
-      return null;
-    }
+    const cacheKey = `${cacheKeyPrefix}:${url}`
+
+    let data = JSON.parse(await redis.get(cacheKey))
 
     if (data) {
-      return data[0] as unknown as APIOutput;
+      return data as unknown as APIOutput;
     }
 
     return null;
@@ -37,8 +38,9 @@ const checkForCache = async (url: string): Promise<APIOutput | null> => {
 
 const createCache = async (data: CacheRecord): Promise<boolean> => {
   try {
-    await supabase.from("meta-cache").insert(data);
-
+    // await supabase.from("meta-cache").insert(data);
+    const cacheKey = `${cacheKeyPrefix}:${data.url}`
+    await redis.set(cacheKey, JSON.stringify(data))
     return true;
   } catch (error) {
     console.log(error);
